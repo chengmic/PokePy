a161e92af3e67fd9df33adf5f0b9eab313c5952c
chengmic <chengmic@oregonstate.edu>
2023-05-10 12:38:55 -0700

    implemented PokeAPI for regions page

--- frames/regions.py
+++ frames/regions.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+import requests
 
 class Regions(ctk.CTkFrame):
     def __init__(self, *args, **kwargs):
@@ -7,11 +8,15 @@ class Regions(ctk.CTkFrame):
         # description
         self.description = ctk.CTkLabel(self, text="Select the region you would like to see Pokémon for.")
 
+        # Call API
+        response = requests.get("https://pokeapi.co/api/v2/region/")
+        data = response.json()
+        regions = data['results']
+
         # location buttons
-        regions = ['Kanto', 'Johto', 'Hoenn']
         self.region_buttons = []
         for region in regions:
-            self.region_buttons.append(ctk.CTkButton(self, text=region, command=self.show_pokemon_list, width=200, height=50))
+            self.region_buttons.append(ctk.CTkButton(self, text=region['name'].title(), command=self.show_pokemon_list, width=200, height=50))
 
         # back button
         self.back_button = ctk.CTkButton(self, text="Go back", command=self.go_back, width=200, height=50)
@@ -22,7 +27,7 @@ class Regions(ctk.CTkFrame):
         self.description.grid(row=row, column=0)
 
         for button in self.region_buttons:
-            button.grid(row=row+1, column=0, pady=10)
+            button.grid(row=row+1, column=0, pady=5)
             row += 1
             
         self.back_button.grid(row=row+2, column=0, pady=10)
