d2ae52017259fb92d2a1da871dc28a1fb41210d1
chengmic <chengmic@oregonstate.edu>
2023-05-16 00:16:53 -0700

    Implemented search functionality: no render, only print test statements

--- frames/search_by_name.py
+++ frames/search_by_name.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+import requests
 
 class SearchByName(ctk.CTkFrame):
     def __init__(self, *args, **kwargs):
@@ -11,17 +12,31 @@ class SearchByName(ctk.CTkFrame):
         self.user_input = ctk.CTkEntry(self, placeholder_text= "Pokémon name", width=200, height=50)
 
         # declare buttons
-        self.search_button = ctk.CTkButton(self, text="Search", width=200, height=50)
+        self.search_button = ctk.CTkButton(self, text="Search", command=self.search_for_pokemon, width=200, height=50)
         self.back_button = ctk.CTkButton(self, text="Go back", command=self.go_back, width=200, height=50)
 
         # place widgets
-        row = 0
-        self.description.grid(row=row, column=0)
-        self.user_input.grid(row=row+1, column=0, pady=10)
-        self.search_button.grid(row=row+2, column=0, pady=10)
-        self.back_button.grid(row=row+3, column=0, pady=10)
+        self.description.grid(row=0, column=0)
+        self.user_input.grid(row=1, column=0, pady=10)
+        self.search_button.grid(row=2, column=0, pady=10)
+        self.back_button.grid(row=3, column=0, pady=10)
 
-    # button callbacks
+    # methods
+    def search_for_pokemon(self):
+        pokemon_name = self.user_input.get().lower()
 
+        if pokemon_name:
+            # call API
+            url = f'https://pokeapi.co/api/v2/pokemon/{pokemon_name}'
+            response = requests.get(url)
+
+            # check if search is valid
+            if response.status_code == 200:
+                data = response.json()
+                print(f"Pokemon found: {data['name']}")
+            else:
+                error_message = f"Pokemon '{pokemon_name}' not found."
+                print(error_message)
+    
     def go_back(self):
         self.master.show_frame('main_menu')
--- frames/search_by_number.py
+++ frames/search_by_number.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+import requests
 
 class SearchByNumber(ctk.CTkFrame):
     def __init__(self, *args, **kwargs):
@@ -9,9 +10,10 @@ class SearchByNumber(ctk.CTkFrame):
 
         # declare input
         self.user_input = ctk.CTkEntry(self, placeholder_text= "Entry no.", width=200, height=50)
+        self.user_input.configure
 
         # declare buttons
-        self.search_button = ctk.CTkButton(self, text="Search", width=200, height=50)
+        self.search_button = ctk.CTkButton(self, text="Search", command=self.search_for_pokemon, width=200, height=50)
         self.back_button = ctk.CTkButton(self, text="Go back", command=self.go_back, width=200, height=50)
 
         # place widgets
@@ -21,7 +23,22 @@ class SearchByNumber(ctk.CTkFrame):
         self.search_button.grid(row=row+2, column=0, pady=10)
         self.back_button.grid(row=row+3, column=0, pady=10)
 
-    # button callbacks
+    # methods
+    def search_for_pokemon(self):
+        pokemon_number = self.user_input.get().lower()
+
+        if pokemon_number:
+            # call API
+            url = f'https://pokeapi.co/api/v2/pokemon/{pokemon_number}'
+            response = requests.get(url)
+
+            # check if search is valid
+            if response.status_code == 200:
+                data = response.json()
+                print(f"Pokemon found: No. {data['id']}: {data['name']}")
+            else:
+                error_message = f"Pokemon '{pokemon_number}' not found."
+                print(error_message)
 
     def go_back(self):
         self.master.show_frame('main_menu')
