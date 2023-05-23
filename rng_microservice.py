2d5e9d00b51962c37c5971d8bf68fcdcff46d3bb
chengmic <chengmic@oregonstate.edu>
2023-05-22 17:59:52 -0700

    added rng_microservice.py

---
+++ rng_microservice.py
@@ -0,0 +1,23 @@
+# ---------- RANDOM NUMBER GENERATOR MICROSERVICE ----------
+
+import socket
+import random
+
+# create a socket at server side
+socket_string = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#bind socket to port
+server_address = ('localhost', 10000)
+socket_string.bind(server_address)
+
+#establish how many clients the server can listen to
+socket_string.listen(1)
+
+while True:
+    conn, addr = socket_string.accept()
+    print("Connected by ", addr)
+    random_num = random.randint(1, 1008)
+    print ('Your randomly generator number is: ', random_num)
+
+    conn.sendall(str(random_num).encode())
+    
+    conn.close()
